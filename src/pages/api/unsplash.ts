6670c9c2be071e82edcefb33ad60c8bba8c76c5c
casiimir <cp.ciancimino@gmail.com>
2024-03-21 11:14:15 +0100

    feat: add unsplash config and utils

---
+++ src/pages/api/unsplash.ts
@@ -0,0 +1,33 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import axios from "axios";
+
+const ACCESS_KEY = process.env.UNSPLASH_ACCESS_KEY;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    res.setHeader("Allow", ["GET"]);
+    return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  try {
+    const response = await axios.get(
+      `https://api.unsplash.com/photos?client_id=${ACCESS_KEY}`
+    );
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      console.error("Failed to fetch Unsplash API:", error.message);
+
+      return res
+        .status(error.response?.status || 500)
+        .json({ message: error.message });
+    }
+
+    console.error("An unexpected error occurred:", error);
+    res.status(500).json({ message: "An unexpected error occurred" });
+  }
+}
