cba8491533f3a659194629637914093c54cd7ab6
casiimir <cp.ciancimino@gmail.com>
2024-03-21 11:42:34 +0100

    feat: add hero component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Hero from "@/components/Hero";
+import Gallery from "@/components/Gallery";
 import styles from "@/styles/pages/home.module.scss";
 
 export default function Home() {
@@ -13,7 +15,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.Homepage}></main>
+      <main className={styles.Homepage}>
+        <Hero />
+        <Gallery />
+      </main>
     </>
   );
 }
